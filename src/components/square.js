33246955221b23e9c166a4571bcab536a91ae713
Kiss György <kissgyorgy@me.com>
2016-11-19 12:49:50 +0100

    Basic components from tutorial

---
+++ src/components/square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Square;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Board from './components/board';
+import Square from './components/square';
 
-class App extends React.Component {
+
+class Game extends React.Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
     );
   }
 }
 
-ReactDOM.render(<App /> , document.querySelector('.container'));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+ReactDOM.render(<Game />, document.querySelector('.container') );
