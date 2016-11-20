fe48a7c29f8dcc89a618d3f05c4549487318818c
Kiss György <kissgyorgy@me.com>
2016-11-20 22:06:07 +0100

    Rewrite Board rendering with two loops

--- src/components/board.js
+++ src/components/board.js
@@ -4,29 +4,22 @@ import Square from './square';
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />;
+    return <Square key={i} value={this.props.squares[i]}
+                   onClick={() => this.props.onClick(i)} />;
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    var rows = [];
+    let squares = [];
+    for (var r = 0; r < 3; r++) {
+      for (var i = r * 3; i < r*3 + 3; i++) {
+        squares.push(this.renderSquare(i));
+      }
+      rows.push(<div key={r} className="board-row">{squares}</div>);
+      squares = [];
+    }
+
+    return <div>{rows}</div>;
   }
 }
 
