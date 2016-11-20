1670302804eb76c0f2971171de38e9fd847c9c6f
Kiss György <kissgyorgy@me.com>
2016-11-20 11:45:28 +0100

    calculate Winner

--- src/components/board.js
+++ src/components/board.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+        return;
+    }
     squares[i] = this.nextPlayer();
     this.setState({
       squares: squares,
@@ -28,8 +31,35 @@ class Board extends React.Component {
     });
   }
 
+  calculateWinner() {
+    const squares = this.state.squares;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = 'Next player: ' + this.nextPlayer();
+    const winner = this.calculateWinner();
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + this.nextPlayer();
+    }
     return (
       <div>
         <div className="status">{status}</div>
