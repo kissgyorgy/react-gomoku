7115bff804878c3bdd5c9f6f055827d7ba8edaa9
Kiss György <kissgyorgy@me.com>
2016-11-21 02:07:10 +0100

    Refact: disabled Navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,41 +2,20 @@ import React from 'react';
 
 
 function Navigation(props) {
-  let nextButton;
-  if (props.stepNumber < props.historyLength - 1) {
-      nextButton = (
-        <button className="pull-xs-right"
-                onClick={() => props.onClick(props.stepNumber + 1)}>
-          Next ⇥
-        </button>
-      );
-  } else {
-      nextButton = (
-        <button disabled="disabled" className="pull-xs-right"
-                onClick={() => props.onClick(props.stepNumber + 1)}>
-          Next ⇥
-        </button>
-      );
-  }
-
-  let prevButton;
-  if (props.stepNumber !== 0) {
-    prevButton = (
+  return (
+    <div>
       <button className="pull-xs-right"
+              disabled={props.stepNumber === props.historyLength - 1}
+              onClick={() => props.onClick(props.stepNumber + 1)}>
+        Next ⇥
+      </button>
+      <button className="pull-xs-right"
+              disabled={props.stepNumber === 0}
               onClick={() => props.onClick(props.stepNumber - 1)}>
         ⇤ Prev
       </button>
-    );
-  } else {
-    prevButton = (
-      <button disabled="disabled" className="pull-xs-right"
-              onClick={() => props.onClick(props.stepNumber - 1)}>
-        ⇤ Prev
-      </button>
-    );
-  }
-
-  return <div> {nextButton} {prevButton} </div>;
+    </div>
+  );
 }
 
 export default Navigation;
